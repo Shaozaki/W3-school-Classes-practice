57a988658b73f2a06bd63105c918705204b55f0e
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-03-31 22:03:54 +0700

    new packages

---
+++ src/inheritance/Tank.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+class Tank {
+    protected String type = "Tiger";
+    public void blast() {
+        System.out.println("C$%82");
+    }
+}
+
+class aws extends Tank {
+    
+}
