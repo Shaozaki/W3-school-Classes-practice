c8f91b79e7395a8cc8cffeac9fab96179fbb123f
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-04-01 17:03:24 +0700

    update new inheritance pack

--- src/inheritance/Tank.java
+++ src/inheritance/Tank.java
@@ -3,10 +3,17 @@ package inheritance;
 class Tank {
     protected String type = "Tiger";
     public void blast() {
-        System.out.println("C$%82");
+        System.out.println("duaaarrrrrra");
     }
 }
 
-class aws extends Tank {
-    
+class Cannon extends Tank {
+    private String modelName = "Samsung";
+    public static void main(String[] args) {
+        Cannon myCannon = new Cannon();
+
+        myCannon.blast();
+
+        System.out.println(myCannon.type+" "+myCannon.modelName);
+    }
 }
